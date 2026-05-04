f255edb47779d4605cb9307430fc747458282b7e
hannahalmuena-lang <Hannahalmuena@gmail.com>
2026-05-05 03:17:00 +0800

    update

--- GROUP3.py
+++ GROUP3.py
@@ -13,3 +13,14 @@ def append():
     file.write(text + "\n")
     file.close()
     print("Entry added.\n")
+def read():
+    try:
+        file = open(FILE_NAME, "r")
+        lines = file.readlines()
+        print("\n--- Entries ---")
+        for line in lines:
+            print(line.strip())
+        print("Total entries:", len(lines))
+        file.close()
+    except:
+        print("No file found.\n")
